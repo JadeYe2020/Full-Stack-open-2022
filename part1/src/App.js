d3cf64a532f456edb8681b5ac66d29586298f61d
Jade Ye <w0430394@nscc.ca>
2022-06-22 15:50:44 -0300

    Finished demo at the end of part1c

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,24 +1,22 @@
-const Hello = ({name, age}) => {
-  const bornYear = () => new Date().getFullYear() - age
+import { useState } from 'react'
+
+const Display = ({counter}) => <div>{counter}</div>
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born in {bornYear()}</p>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={decreaseByOne} text="minus" />
+      <Button onClick={setToZero} text="zero" />
     </div>
-  )
-}
-
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </>
   )
 }
 
